b032dabc91e2b583d96a113a775e13f94b6b6c21
Zach Zhong <zzyzsy0516321@yahoo.com>
2024-05-14 18:26:47 -0700

    #35 add scrollable zoo map

--- app/src/main/java/com/example/zooseeker_jj_zaaz_team_52/ui/Map/MapFragment.java
+++ app/src/main/java/com/example/zooseeker_jj_zaaz_team_52/ui/Map/MapFragment.java
@@ -2,8 +2,10 @@ package com.example.zooseeker_jj_zaaz_team_52.ui.Map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,10 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.zooseeker_jj_zaaz_team_52.databinding.FragmentHomeBinding;
-
 public class MapFragment extends Fragment {
-
     private FragmentHomeBinding binding;
+    private ImageView mapView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float scaleFactor = 1.0f;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +26,8 @@ public class MapFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        mapView = binding.map;
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -34,7 +36,4 @@ public class MapFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-
-
 }
